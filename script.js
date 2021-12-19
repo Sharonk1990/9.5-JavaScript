e65b88ab6314f8e10530694bef21c4c77574237f
Sharonk1990 <92334366+Sharonk1990@users.noreply.github.com>
2021-12-19 15:34:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const makePizza = function () {
+    console.log("Maak het deeg klaar");
+    console.log("Rol het deeg uit");
+    console.log("Beleg het deeg met pastasaus en kaas");
+    console.log("Doe de pizza in de oven");
+    console.log("Na 30 minuten is de pizza klaar!")
+};
+
+const makeSushi = function () {
+    console.log("kook de rijst");
+    console.log("Leg de rijst in een norivel");
+    console.log("Doe de vis en groenten in het midden");
+    console.log("Rol de sushi op");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
